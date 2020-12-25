5f7364e693402ff0c618373981a8657774d3b258
freebomd <76235334+freebomd@users.noreply.github.com>
2020-12-25 12:45:50 +0530

    Add files via upload

---
+++ firstapi.py
@@ -0,0 +1,28 @@
+import requests
+import subprocess
+import sys
+
+title_art = r"""
+
+ ######  ##     ## ########  #### ##    ## ##    ## ##     ## ########     ####  #######  
+##    ## ##     ## ##     ##  ##  ###   ## ##   ##  ###   ### ##            ##  ##     ## 
+##       ##     ## ##     ##  ##  ####  ## ##  ##   #### #### ##            ##  ##     ## 
+ ######  ######### ########   ##  ## ## ## #####    ## ### ## ######        ##  ##     ## 
+      ## ##     ## ##   ##    ##  ##  #### ##  ##   ##     ## ##            ##  ##     ## 
+##    ## ##     ## ##    ##   ##  ##   ### ##   ##  ##     ## ##       ###  ##  ##     ## 
+ ######  ##     ## ##     ## #### ##    ## ##    ## ##     ## ######## ### ####  #######  """
+print(title_art+'\n')
+
+
+def main():
+
+    movie_name = input("Enter Your Long Url:\n")
+    print(f"Searching for {movie_name}")
+    base_url = f"https://shrinkme.io/api?api=9b8aab2630a0e5c289837488ea83536e6912dd99&url={movie_name}"
+    torrent_results = requests.get(url=base_url).json()
+    data = torrent_results
+    index = 0
+    print("Your Shot Url= "+data['shortenedUrl']+'\n'+"Created By Seshu Sai")
+
+if __name__ == "__main__":
+    main()
